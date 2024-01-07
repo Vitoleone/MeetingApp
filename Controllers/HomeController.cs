9ab68ecd32b0025e98ff4bf2f625f1a8688682b7
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2024-01-08 00:33:39 +0300

    ViewBag added for layout title

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,7 @@ namespace MeetingApp
     {
         public IActionResult Index()
         {
+            ViewBag.Title = "Homepage";
             return View();
         }
     }
--- Controllers/MeetingController.cs
+++ Controllers/MeetingController.cs
@@ -6,15 +6,18 @@ namespace MeetingApp
     {
         public IActionResult Index()
         {
+            ViewBag.Title = "Meeting Page";
             return View();
         }
         public IActionResult Apply()
         {
+            ViewBag.Title = "Apply";
             return View();
         }
 
         public IActionResult List()
         {
+            ViewBag.Title = "User List";
             return View();
         }
     }
