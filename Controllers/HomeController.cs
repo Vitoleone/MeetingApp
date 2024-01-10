aef7500a1035143c35440a60a661cc131d5935c6
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2024-01-10 22:01:08 +0300

    Add userInfo on Apply page to repository

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,11 +7,12 @@ namespace MeetingApp
     {
         public IActionResult Index()
         {
+            int UserCount = Repository.Users.Where(i=>i.WillAttend == true).Count();
             var meetingInfo = new MeetingInfo(){
                 Id = 1,
                 Location ="Istanbul, Unity Kongresi",
                 Date = new DateTime(2024,12,1,15,15,0),
-                NumberOfPeople = 100
+                NumberOfPeople = UserCount
             };
             return View(meetingInfo);
         }
--- Controllers/MeetingController.cs
+++ Controllers/MeetingController.cs
@@ -5,11 +5,6 @@ namespace MeetingApp
 {
     public class MeetingController : Controller
     {
-        public IActionResult Index()
-        {
-            ViewBag.Title = "Meeting Page";
-            return View();
-        }
         [HttpGet]
         public IActionResult Apply()
         {
@@ -19,14 +14,16 @@ namespace MeetingApp
         [HttpPost]
         public IActionResult Apply(UserInfo userInfo)
         {
-            ViewBag.Title = "Apply";
-            return View();
+            ViewBag.Title = "Başvuru Durumu";
+            Repository.CreateUser(userInfo);
+            ViewBag.UserCount = Repository.Users.Where(i=>i.WillAttend == true).Count();
+            return View("Thanks",userInfo);
         }
 
         public IActionResult List()
         {
             ViewBag.Title = "User List";
-            return View();
+            return View(Repository.Users);
         }
     }
 }
