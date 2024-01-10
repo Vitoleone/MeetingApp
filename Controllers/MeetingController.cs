e18486364b529ad58de8e744a474d536fda53ce9
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2024-01-10 20:53:25 +0300

    Form page adjusted

--- Controllers/MeetingController.cs
+++ Controllers/MeetingController.cs
@@ -9,7 +9,14 @@ namespace MeetingApp
             ViewBag.Title = "Meeting Page";
             return View();
         }
+        [HttpGet]
         public IActionResult Apply()
+        {
+            ViewBag.Title = "Apply";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Apply(string Name, string Phone, string Email, bool WillAttend)
         {
             ViewBag.Title = "Apply";
             return View();
